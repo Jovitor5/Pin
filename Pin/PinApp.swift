3b3533f905f076be6d3e06f870654b7f8f8ad83e
Joao <moreira.jv11@gmail.com>
2024-07-03 15:00:02 +0200

    Initial Commit

---
+++ Pin/PinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinApp.swift
+//  Pin
+//
+//  Created by João Vitor Moreira on 03.07.24.
+//
+
+import SwiftUI
+
+@main
+struct PinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
